35d44f57a765fe29dbf1d28191d06c6200e830a0
Debanjan2000 <debanjandas340@gmail.com>
2023-07-09 18:51:19 +0530

    drumkit added

---
+++ codes/DRUMKIT/dist/index.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var no_of_drum_buttons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < no_of_drum_buttons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("i am clicked!!");
+  });
+}
